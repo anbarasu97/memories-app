65e8e7929671acdd3b3d4448b0f0c28c7bac875d
anbarasu97 <62258376+anbarasu97@users.noreply.github.com>
2022-03-24 10:44:36 +0530

    Initial commit

---
+++ client/build/precache-manifest.1af6a455809b7c9386ba842c1c2fc963.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f295ed49dfb627f4fed9122ae137643",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b619ec771a3396db6e3",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "c0760040102295bd88f5",
+    "url": "/static/js/2.22f9ae28.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.22f9ae28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b619ec771a3396db6e3",
+    "url": "/static/js/main.ef4f2e7e.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
